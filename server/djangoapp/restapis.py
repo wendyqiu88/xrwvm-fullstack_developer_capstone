89ffb4681d080b35de44c4c15680b47ab24c6967
Wendy Qiu <bellaqiu0329@yahoo.com>
2024-09-30 08:23:24 -0700

    Your commit message here

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -14,26 +14,6 @@ sentiment_analyzer_url = os.getenv(
 # def get_request(endpoint, **kwargs):
 # Add code for get requests to back end
 
-# def analyze_review_sentiments(text):
-# request_url = sentiment_analyzer_url+"analyze/"+text
-# Add code for retrieving sentiments
-
-# def post_review(data_dict):# Uncomment the imports below before you add the function code
-# import requests
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-backend_url = os.getenv(
-    'backend_url', default="http://localhost:3030")
-sentiment_analyzer_url = os.getenv(
-    'sentiment_analyzer_url',
-    default="http://localhost:5050/")
-
-# def get_request(endpoint, **kwargs):
-# Add code for get requests to back end
-
 def get_request(endpoint, **kwargs):
     params = ""
     if(kwargs):
@@ -75,6 +55,4 @@ def post_review(data_dict):
         return response.json()
     except:
         print("Network exception occurred")
-# Add code for posting review
-
 # Add code for posting review
