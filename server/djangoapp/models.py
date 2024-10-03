1341e297bc530e753687eb14e4567c717fb45bcc
Wendy Qiu <bellaqiu0329@yahoo.com>
2024-10-03 12:54:09 -0700

    Your commite message here

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -5,26 +5,43 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 class CarMake(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
-    # Other fields as needed
+    country_of_origin = models.CharField(
+        max_length=100, blank=True, null=True
+    )
+    founded_year = models.PositiveIntegerField(
+        blank=True, null=True
+    )
+    website = models.URLField(
+        blank=True, null=True
+    )
+    headquarters = models.CharField(
+        max_length=255, blank=True, null=True
+    )
 
     def __str__(self):
-        return self.name  # Return the name as the string representation
+        return self.name
 
 
 class CarModel(models.Model):
-    # Many-to-One relationship
-    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
+    car_make = models.ForeignKey(
+        CarMake, on_delete=models.CASCADE
+    )
     name = models.CharField(max_length=100)
+
     CAR_TYPES = [
         ('SEDAN', 'Sedan'),
         ('SUV', 'SUV'),
         ('WAGON', 'Wagon'),
-        # Add more choices as required
+        ('COUPE', 'Coupe'),
+        ('CONVERTIBLE', 'Convertible'),
+        ('HATCHBACK', 'Hatchback'),
+        ('TRUCK', 'Truck'),
+        ('VAN', 'Van'),
+        ('SPORT', 'Sport'),
+        ('ELECTRIC', 'Electric'),
     ]
     type = models.CharField(
-        max_length=10,
-        choices=CAR_TYPES,
-        default='SUV'
+        max_length=12, choices=CAR_TYPES, default='SUV'
     )
     year = models.IntegerField(
         default=2023,
@@ -33,8 +50,28 @@ class CarModel(models.Model):
             MinValueValidator(2015)
         ]
     )
-    # Other fields as needed
-    def __str__(self):
-        return self.name  # Return the name as the string representation
 
-# Newline added at the end of the file
+    engine = models.CharField(
+        max_length=50, blank=True, null=True
+    )
+    transmission = models.CharField(
+        max_length=20,
+        choices=[('Automatic', 'Automática'), ('Manual', 'Manual')],
+        blank=True
+    )
+    fuel_type = models.CharField(
+        max_length=15,
+        choices=[
+            ('Gasoline', 'Gasolina'),
+            ('Diesel', 'Diésel'),
+            ('Electric', 'Eléctrico'),
+            ('Hybrid', 'Híbrido')
+        ],
+        blank=True
+    )
+    price = models.DecimalField(
+        max_digits=10, decimal_places=2, blank=True, null=True
+    )
+
+    def __str__(self):
+        return f"{self.car_make.name} {self.name} ({self.year})"
