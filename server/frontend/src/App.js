b0aa37f416279a93c9f8f6228a9bbed879a9d15b
Wendy Qiu <bellaqiu0329@yahoo.com>
2024-10-03 12:00:23 -0700

    Your commite message here

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -3,10 +3,7 @@ import Register from "./components/Register/Register";
 import { Routes, Route } from "react-router-dom";
 import Dealers from './components/Dealers/Dealers';
 import Dealer from "./components/Dealers/Dealer";
-<<<<<<< HEAD
 import PostReview from "./components/Dealers/PostReview";
-=======
->>>>>>> cd59725c55cb7fdd39135fd231953589c0006a93
 
 function App() {
   return (
@@ -15,10 +12,7 @@ function App() {
       <Route path="/register" element={<Register />} />
       <Route path="/dealers" element={<Dealers/>} />
       <Route path="/dealer/:id" element={<Dealer/>} />
-<<<<<<< HEAD
       <Route path="/postreview/:id" element={<PostReview/>} />
-=======
->>>>>>> cd59725c55cb7fdd39135fd231953589c0006a93
     </Routes>
   );
 }
